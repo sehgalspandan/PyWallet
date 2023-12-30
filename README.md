7e00925aeca61ec827708304abea6c5076386b8f
Spandan <spandansehgal@gmail.com>
2023-12-30 20:30:25 +0530

    Refactor expense tracker code and add features (delete, history)

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Expense-Tracker-In-Python
+# Expense Tracker
+
+This Python project is an expense tracker that allows users to add, view, and delete expenses. It uses SQLite for database storage and provides a command-line interface for interaction.
+
+## Features
+
+1. **Add Expense**: Users can add new expenses by providing a title, amount, and category.
+2. **Print Expense History**: View the history of all expenses, including their details such as ID, title, date, amount, and category.
+3. **Delete Expense**: Remove an expense from the history by specifying its ID.
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you have Python installed on your system.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yourusername/expense-tracker.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd expense-tracker
+   ```
+
+3. Run the script:
+
+   ```bash
+   python expense_tracker.py
+   ```
+
+## Usage
+
+1. Choose the appropriate option by entering the corresponding number.
+2. Follow the prompts to add, view, or delete expenses.
+
+## Files
+
+- **`expense_tracker.py`**: The main Python script containing the Expense class and the logic for user interaction.
+- **`greetings.json`**: A JSON file containing a list of greetings used randomly.
+- **`expenses.db`**: SQLite database file storing the expenses.
+
+## Acknowledgments
+
+- This project was inspired by the need for a simple command-line expense tracker.
+
+## Tech Stack
+
+- Python
+- Sqlite3
--- main.py
+++ main.py
@@ -4,18 +4,10 @@ import json
 import random
 
 
-# Initializing the database
-connection = sqlite3.connect("expenses.db")
-cursor = connection.cursor()
-
-
-
-
 class Expense:
-    def __init__(self, title, amount, category):
-        current_time = datetime.datetime.now()
+    def __init__(self, title, amount, category, date=None):
         self.title = title
-        self.date = current_time.strftime("%d/%m/%Y")
+        self.date = date or datetime.datetime.now().strftime("%d/%m/%Y")
         self.amount = amount
         self.category = category
 
@@ -30,40 +22,90 @@ def get_random_greeting():
         return random.choice(greetings_list)
 
 
+def insert_expense(cursor, expense):
+    cursor.execute(
+        "INSERT INTO expenses (title, date, amount, category) VALUES (?, ?, ?, ?)",
+        (expense.title, expense.date, expense.amount, expense.category),
+    )
+    
+    connection.commit()
+
+
+def print_history(cursor):
+    rows = cursor.execute("SELECT * FROM expenses").fetchall()
+
+    if not rows:
+        print("Empty! Add an expense to get started.")
+    else:
+        print("\nExpense History:")
+        for row in rows:
+            print(
+                f"ID: {row[0]}, Title: {row[1]}, Date: {row[2]}, Amount: ₹{row[3]}, Category: {row[4]}"
+            )
+
+def delete_expense(cursor, id):
+    
+    
+    cursor.execute("DELETE FROM expenses WHERE ID=?", (id,))
+    
+    cursor.execute("UPDATE expenses SET ID = ID - 1 WHERE ID > ?", (id,))
+    
+    cursor.execute("UPDATE SQLITE_SEQUENCE SET SEQ=SEQ-1 WHERE NAME='expenses'")
+    
+    connection.commit()
+
+
+
 try:
+    connection = sqlite3.connect("expenses.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS expenses (ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date TEXT, amount REAL, category TEXT)"
+    )
 
     print(get_random_greeting())
     print("-------------------------------")
 
-
     while True:
-        title = input("\nEnter the title of the expense: ")
-        amount = input("Enter the amount of the expense: ")
-        category = input("Enter the category of the expense: ")
+        print("\nWhat do you want to do? (Enter the number)")
+        print("1. Add Expense")
+        print("2. Print Expense History")
+        print("3. Delete Expense")
+        print("4. Exit")
 
-        new_expense = Expense(title, amount, category)
+        choice = input("Enter your choice: ")
 
-        e_title = new_expense.title
-        e_date = new_expense.date
-        e_amount = new_expense.amount
-        e_category = new_expense.category
+        if choice == "1":
+            title = input("\nEnter the title of the expense: ")
+            while True:
+                amount = input("Enter the amount of the expense: ")
+                try:
+                    amount = float(amount)
+                    break
+                except ValueError:
+                    print("Invalid amount. Please enter a number.")
+            category = input("Enter the category of the expense: ")
 
-        cursor.execute("CREATE TABLE IF NOT EXISTS expenses (ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date TEXT, amount REAL, category TEXT)")
-        cursor.execute("INSERT INTO expenses (title, date, amount, category) VALUES (?, ?, ?, ?)", (e_title, e_date, e_amount, e_category))
+            new_expense = Expense(title, amount, category)
+            insert_expense(cursor, new_expense)
 
-        rows = cursor.execute("SELECT * FROM expenses").fetchall()
-        print("\nExpense History:")
-        for row in rows:
-            print(f"ID: {row[0]}, Title: {row[1]}, Date: {row[2]}, Amount: ₹{row[3]}, Category: {row[4]}")
+            print("\n" + str(new_expense))
 
-        print("\n" + str(new_expense))
+        elif choice == "2":
+            print_history(cursor)
 
-        connection.commit()
+        elif choice == "3":
+            print_history(cursor)
+            id_to_delete = input("\nEnter the ID of the expense to delete: ")
+            delete_expense(cursor, id_to_delete)
+            print("\nExpense deleted successfully!")
 
-        add_new = input("Do you want to add another expense? (yes/no): ")
-        if add_new.lower() != 'yes':
+        elif choice == "4":
             print("\nExiting...")
             break
+
+        else:
+            print("Invalid choice. Please try again.")
 
 except KeyboardInterrupt:
     print("\nExiting...")
